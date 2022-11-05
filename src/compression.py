4bb6445b3e32bfcbd7c554eba53aa2fa7b4952e9
Pimrak Apiratchatanont <apimrak@gmail.com>
2022-11-05 18:58:14 +0700

    Modify compression algorithm

--- src/compression.py
+++ src/compression.py
@@ -85,3 +85,91 @@ class RunLength():
       cnt += 1
       result.append(a * b)
     return "".join(result)
+
+class DnaRunLength():
+  mp = {
+      'A': 0,
+      'T': 1,
+      'C': 2,
+      'G': 3,
+      chr(0): 4,
+      chr(1): 5,
+  }
+  inv = {v: k for k, v in mp.items()}
+
+  def encode(self, s):
+    ofcnt = 0
+    current = s[0]
+    count = 0
+    result = []
+    for i in range(1, len(s)):
+      if s[i] != current or count == 31:
+        if count == 31:
+          ofcnt += 1
+        result.append(chr(self.mp[current] << 5 | count))
+        current = s[i]
+        count = 0
+      else:
+        count += 1
+    print(ofcnt)
+    result.append(chr(self.mp[current] << 5 | count))
+    return ''.join(result)
+
+  def decode(self, s):
+    result = []
+    for i in s:
+      char = ord(i) >> 5
+      count = ord(i) & 31
+      result.append((count + 1) * self.inv[char])
+    return ''.join(result)
+
+class SplitPart():
+  def encode(self, s):
+    Dna = []
+    Label = []
+    for line in s.split('\n'):
+      if line[0] == '>':
+        Label.append(line)
+        Dna.append("")
+      else:
+        Dna[len(Dna) - 1] += line
+    Label = chr(1).join(Label)
+    Dna = chr(1).join(Dna)
+    return [Dna, Label]
+
+  def decode(self, Dna, Label):
+    Dna = Dna.split(chr(1))
+    Label = Label.split(chr(1))
+    result = []
+    for i in range(len(Label)):
+      result.append(Label[i])
+      result.append(Dna[i])
+    return '\n'.join(result)
+
+
+class Compress():
+  bwt = BWT()
+  runlength = RunLength()
+  dnarunlength = DnaRunLength()
+  splitPart = SplitPart()
+
+  def encode(self, s):
+    [Dna, Label] = self.splitPart.encode(s)
+
+    encodeDna = self.bwt.encode(Dna)
+    encodeDna = self.dnarunlength.encode(encodeDna)
+
+    encodeLabel = self.bwt.encode(Label)
+    encodeLabel = self.runlength.encode(encodeLabel)
+
+    return [encodeDna, encodeLabel]
+
+  def decode(self, Dna, Label):
+
+    Dna = self.dnarunlength.decode(Dna)
+    Dna = self.bwt.decode(Dna)
+
+    Label = self.runlength.decode(Label)
+    Label = self.bwt.decode(Label)
+
+    return self.splitPart.decode(Dna, Label)
